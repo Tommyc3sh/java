b5323a748ea280ff7cd252fb171fc5241546de6a
Tommyc3sh <75736313+Tommyc3sh@users.noreply.github.com>
2021-06-08 10:42:29 +0300

    Add files via upload

---
+++ h9.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream; 
+
+public class h9 {
+
+	public static void main(String[] args) {
+		IntStream.range(1, 67).forEachOrdered(n -> {
+		    System.out.println(n);
+		});
+
+	}
+
+}
---
+++ harjutus17.java
@@ -0,0 +1,18 @@
+public class harjutus17 {
+	public static void main(String args[])   
+	{  
+	int number=40;  
+	System.out.print("Numbrid 1 kuni"+number+": ");  
+	for (int i=1; i<=number; i++)   
+	{  
+	if (i%2==0)   
+	{  
+	System.out.print(i + " ");  
+	}  
+	}  
+	}  
+	}  
+	
+	
+	
+	
